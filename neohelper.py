4a0fb3ce8598a09d37637f7f0db39c0e4fb19592
newmanrs <newmanrs@umich.edu>
2021-09-24 19:20:53 -0700

    refactoring to use generic query endpoint

--- neohelper.py
+++ neohelper.py
@@ -32,27 +32,21 @@ def cli(ctx,uri,db_pw_env_var):
 @cli.command()
 @click.pass_context
 def count(ctx):
-    """ Display count nodes in database """
-    driver = ctx.obj['driver']
+    """ Display count of nodes and edges database """
 
-    #Call Driver, _count, and read transaction...
-    with driver.session() as session:
-        srt = driver.session().read_transaction
-        c = srt(_count)
-
-def _count(tx):
     query = """
         OPTIONAL MATCH (n) with count(n) as node_count
         OPTIONAL MATCH ()-[r]->()
         with node_count, count(r) as edge_count
         RETURN {node_count : node_count, edge_count : edge_count} as counts
     """
-    records = tx.run(query)
-    counts = records.single()['counts']
-    nc = counts['node_count']
-    ec = counts['edge_count']
-    click.echo(f"Database contains {nc} nodes and {ec} relationships.")
-    return counts
+
+    result = _query(ctx, query)
+    nc = result['counts']['node_count']
+    ec = result['counts']['edge_count']
+    click.echo(f"Database contains {nc} nodes and {ec} relationships")
+
+
 
 @cli.command()
 @click.pass_context
@@ -61,21 +55,16 @@ def _count(tx):
 def count_labels(ctx, *args, **kwargs):
     """ Count of each node label """
 
-    driver = ctx.obj['driver']
-    with driver.session() as session:
-        srt = driver.session().read_transaction
-        label_counts = srt(_count_labels,*args, **kwargs)
-
-def _count_labels(tx, *args, **kwargs):
     query = """
     call db.labels() yield label
     match (n) where label in labels(n)
     with label, count(n) as count order by count DESC
     return collect({label : label, count : count}) as label_counts
     """
-    records = tx.run(query)
-    results = records.single()['label_counts']
+    record = _query(ctx,query)
     labels = kwargs['labels']
+    results = record['label_counts']
+
     if labels is not None:
         labels = labels.split()
         lcl = []
@@ -98,8 +87,6 @@ def _count_labels(tx, *args, **kwargs):
             c = lc['count']
             click.echo(f"{l} : {c}")
 
-    return results
-
 @cli.command()
 @click.pass_context
 @click.argument('query',type=str)
@@ -110,19 +97,34 @@ def _count_labels(tx, *args, **kwargs):
     show_default = True)
 @click.option('--json','-j',
     multiple=True)
+@click.option('--verbose', '-v',
+    default = False,
+    is_flag = True)
 def query(ctx, *args, **kwargs):
     """ Perform given cypher query """
-    l = []
-    for param in kwargs['json']:
-        l.append(json.loads(param))
+
     query = kwargs['query']
     mode = kwargs['mode']
+    jsons = kwargs['json']
+    verbose = kwargs['verbose']
+
+    l = []
+
+    if verbose:
+        click.echo("Input query is: \n{}".format(query))
+
+    for j in jsons:
+        if verbose:
+            click.echo("Parsing json parameters:")
+            click.echo(j)
+        l.append(json.loads(j))
 
     results = _query(ctx, query, l, mode)
-    for row in results:
-        click.echo(row)
+    #for row in results:
+    #    click.echo(row)
+    #click.echo(results)
 
-def _query(ctx, query, params, mode):
+def _query(ctx, query, params =[], mode = 'read'):
     with ctx.obj['driver'].session() as session:
         if mode == 'read':
             txfn = session.read_transaction
@@ -132,35 +134,30 @@ def _query(ctx, query, params, mode):
 
 def _tx_func(tx, query, params):
     if params:
-        print(query)
-        print(params)
         results = tx.run(query,params=params)
     else:
         results = tx.run(query)
     l = []
     for r in results:
-        click.echo(r)
+        #click.echo(r)
         keys = r.keys()
         values = r.values()
         d = dict()
         for k,v in zip(keys,values):
             d[k]=v
         l.append(d)
-
+    if len(l) == 0:
+        return None
+    elif len(l) == 1:
+        return l[0]
     return l
 
 @cli.command()
 @click.pass_context
 def detach_delete(ctx):
     """  Delete all nodes and relationships in the database """
-    driver = ctx.obj['driver']
-    with driver.session() as session:
-        swt = driver.session().write_transaction
-        swt(_detach_delete)
-        swt(_count)
 
-def _detach_delete(tx):
     query = """
     MATCH (n) DETACH DELETE (n)
     """
-    tx.run(query)
+    _query(ctx, query, mode='write')
